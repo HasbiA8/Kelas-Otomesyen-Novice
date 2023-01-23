12f27f9a4470a6dde1f82c7a1fbdd490d38408d2
Hasbi Anshori <94293460+HasbiA8@users.noreply.github.com>
2023-01-23 14:02:23 +0800

    Tugas Pertemuan 1

---
+++ Tugas1.py
@@ -0,0 +1,146 @@
+#Loops
+#While
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [1,2,3,4,5]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+#For
+x = [6,7,8,9,10]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [11,12,13,14,15]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [16,17,18,19,20]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [21,22,23,24,25]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [26,27,28,29,30]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [31,32,33,34,35]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [36,37,38,39,40]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [41,42,43,44,45]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dang")
+    x = x+1
+
+#For
+x = [46,47,48,49,50]
+for i in x:
+    print(i)
+
+x = 1
+while  x <2:
+    print("Dut")
+    x = x+1
+
+x = 1
+while  x <2:
+    print("Dangdut")
+    x = x+1
